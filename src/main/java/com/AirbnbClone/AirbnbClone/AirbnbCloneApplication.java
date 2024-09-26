00642a00f24f44157c6174360301f4864342aed0
siddharthsharma <siddharth54330@gmail.com>
2024-09-27 00:25:56 +0530

    init commit

---
+++ src/main/java/com/AirbnbClone/AirbnbClone/AirbnbCloneApplication.java
@@ -0,0 +1,13 @@
+package com.AirbnbClone.AirbnbClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AirbnbClone/AirbnbClone/AirbnbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AirbnbClone.AirbnbClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
